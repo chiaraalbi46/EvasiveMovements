4cb77b7100d052c5dd3c03cf0e4c11b6cd4ffee0
Chiara Albisani <chiaraalbi46@gmail.com>
2021-09-06 14:39:53 +0200

    Control test.py outputs.

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ import torch
 # from trajectory_images import save_video
 from configs.config import cfg
 import numpy as np
+from create_video_json import write_json
 
 
-def test(model, criterion, model_path, test_loader, paths, dev, model_type):
+# def test(model, criterion, model_path, test_loader, paths, dev, model_type):
+def test(test_loader, paths, dev, model_type):
     num_correct = 0
     test_losses = []
     distances = []
@@ -14,45 +16,62 @@ def test(model, criterion, model_path, test_loader, paths, dev, model_type):
     csv_file = cfg.SAVE_RESULTS_PATH[model_type] + "summarize_test.csv"
     # video_name = dir_name + cfg.SAVE_VIDEO_PATH[model_type] + "summarize_video.avi"
 
-    if os.path.exists(csv_file):
-        os.remove(csv_file)
-
-    model.load_state_dict(torch.load(model_path))
-
-    # device = torch.device('cuda:' + str(dev))
-    device = torch.device('cuda')
-    model.to(device)
-    model.eval()
+    # if os.path.exists(csv_file):
+    #     os.remove(csv_file)
+    #
+    # model.load_state_dict(torch.load(model_path))
+    #
+    # # device = torch.device('cuda:' + str(dev))
+    # device = torch.device('cuda')
+    # model.to(device)
+    # model.eval()
 
     print("starting testig the model")
 
     current_path = 0
+    array_json = []
+    for inputs, labels in test_loader:
+        print("INPUTS: ", inputs)
+        print("LABELS: ", labels)
+        for i in range(len(inputs)):
+            real = labels[i]  # .detach().numpy()
+            path = paths[current_path].replace("left_frames_processed", "left_frames")
 
-    with open(csv_file, 'w') as result_file:
-        with torch.no_grad():
-            filewriter = csv.writer(result_file)
-            for inputs, labels in test_loader:
+            # lines = [path, predicted.tolist(), real.tolist()]
+            # filewriter.writerow(lines)
+            dict_frame = {'Ind': i, 'real': real, 'path': path}
+            array_json.append(dict_frame)
+            current_path += 1
 
-                image = inputs.to(device)
-                output = model(image.float(), device)
+    write_json(array_json, '/home/aivdepth/PROVA_OUTPUT_TEST.json')
 
-                test_loss = criterion(output, labels.float())
+    # with open(csv_file, 'w') as result_file:
+        # with torch.no_grad():
+            # filewriter = csv.writer(result_file)
+            # for inputs, labels in test_loader:
+            #     print("INPUTS: ", inputs)
+            #     print("LABELS: ", labels)
 
-                test_losses.append(test_loss.item())
+                # image = inputs.to(device)
+                # output = model(image.float(), device)
+                #
+                # test_loss = criterion(output, labels.float())
+                #
+                # test_losses.append(test_loss.item())
 
-                for i in range(len(output)):
-                    predicted = output[i].detach().numpy()
-                    real = labels[i].detach().numpy()
-                    path = paths[current_path].replace("left_frames_processed", "left_frames")
-                    lines = [path, predicted.tolist(), real.tolist()]
-                    filewriter.writerow(lines)
-                    current_path += 1
+                # for i in range(len(output)):
+                #     predicted = output[i].detach().numpy()
+                #     real = labels[i].detach().numpy()
+                #     path = paths[current_path].replace("left_frames_processed", "left_frames")
+                #     lines = [path, predicted.tolist(), real.tolist()]
+                #     filewriter.writerow(lines)
+                #     current_path += 1
 
-    print()
-    print("saved resume csv file in " + csv_file)
-    print()
-    print("Test loss: {:.3f}".format(np.mean(test_losses)))
-    print('*' * 100)
-    print('Starting Video Creation')
+    # print()
+    # print("saved resume csv file in " + csv_file)
+    # print()
+    # print("Test loss: {:.3f}".format(np.mean(test_losses)))
+    # print('*' * 100)
+    # print('Starting Video Creation')
 
     # save_video(video_name=video_name, csv_path=csv_file, len_seq=cfg.TEST.LEN_SEQUENCES, model_type=model_type)
