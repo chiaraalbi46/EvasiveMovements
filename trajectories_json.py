84425018686e4370bce5f20c1678b025d884ed96
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2021-09-16 10:19:22 +0200

    Fix.

--- trajectories_json.py
+++ trajectories_json.py
@@ -1,3 +1,4 @@
+##
 import argparse
 import json
 import os
@@ -16,7 +17,7 @@ def transform_RT(xdata, zdata, i, origin, angle_o, f, count):
             zdata[i + count] - origin[f][1]) * math.sin(angle_o[f])
     y_rot = (xdata[i + count] - origin[f][0]) * math.sin(angle_o[f]) + (
             zdata[i + count] - origin[f][1]) * math.cos(angle_o[f])
-    print('i', i, 'count', count)
+   # print('i', i, 'count', count)
     # Traslazione
     x_rot_t = x_rot + origin[f][0]
     y_rot_t = y_rot + origin[f][1]
@@ -27,18 +28,23 @@ def transform_RT(xdata, zdata, i, origin, angle_o, f, count):
 
 
 # array origine
-def origin_traj(i_start, xdata, zdata, angle, origin_distance):
+def origin_traj(i_start, xdata, zdata, angle, origin_distance, frame_index):
     # Origini
     origin = []
     angle_o = []
+    origin_index = []
+    origine_i = []
     while i_start < len(xdata):
         origin.append([xdata[i_start], zdata[i_start]])
         angle_o.append(angle[i_start])
+        origin_index.append(frame_index[i_start])
+        origine_i.append(i_start)
         i_start += origin_distance  # num_vectors
-    return origin, angle_o
+
+    return origin, angle_o, origin_index, origine_i
 
 
-def create_traj_json(video_json_path, i_start, point_past, point_future, origin_distance, dest_folder):
+def create_traj_json(video_json_path, step, i_start, point_past, point_future, origin_distance, dest_folder):
 
     point_future = point_future + 1  # +1 punto futuro (origine da escludere)
 
@@ -47,17 +53,16 @@ def create_traj_json(video_json_path, i_start, point_past, point_future, origin_
         xdata = []
         zdata = []
         angle = []
+        frame_index = []
 
-        # step = 0
         for i in range(len(d)):
-            # if i == step:
             xdata.append(d[i]['cords'][0])
             zdata.append(d[i]['cords'][2])
             angle.append(d[i]['angle'])
-            #     step += sample
+            frame_index.append(d[i]['Frame'])
 
     # angolo dei soli origini
-    origin, angle_o = origin_traj(i_start, xdata, zdata, angle, origin_distance)  # crea origini
+    origin, angle_o, origin_index, origine_i = origin_traj(i_start, xdata, zdata, angle, origin_distance, frame_index)  # crea origini
 
     # futuro, presente, passato
     future = []
@@ -68,7 +73,8 @@ def create_traj_json(video_json_path, i_start, point_past, point_future, origin_
 
     f = 0
     for i in range(len(xdata)):
-        if xdata[i] == origin[f][0] and zdata[i] == origin[f][1] and f < len(origin) - 1 and i % origin_distance == 0:
+
+       if origine_i[f] == i and f < len(origin) - 1 : #(i*step ) == origin_index[f] , and xdata[i] == origin[f][0] and zdata[i] == origin[f][1] and f < len(origin) - 1 :#and frame_index[i] % origin_distance == 0:
             present.append(origin[f])
 
             # Punti futuri
@@ -80,13 +86,14 @@ def create_traj_json(video_json_path, i_start, point_past, point_future, origin_
 
             # Punti passati
             count = point_past
-            while i - count > 0 and count > 0:
+            while i - count >= 0 and count > 0:# and i - count != 0:
+
                 new_point = transform_RT(xdata, zdata, i, origin, angle_o, f, (-count))
                 past.append(new_point)
                 count -= 1
 
             if len(past) == point_past and len(future) == point_future - 1:  # se hanno meno punti non salvo
-                dic_traj['Frame'] = i  # indice dell'origine
+                dic_traj['Frame'] = origin_index[f] # indice dell'origine
                 dic_traj['Past'] = past
                 dic_traj['Present'] = present
                 dic_traj['Future'] = future
@@ -98,18 +105,19 @@ def create_traj_json(video_json_path, i_start, point_past, point_future, origin_
             past = []
             f += 1
 
-        spl = video_json_path.split(os.sep)  # '/'
-        vid_name = spl[len(spl) - 1]
-        spl1 = vid_name.split('.')
-        vname = spl1[0]
-        final_name = vname + '_traj.json'
-        pathToTrajFile = dest_folder + final_name  # devo avere messo lo slah in pathToTrajDir !
+       spl = video_json_path.split(os.sep)  # '/'
+       vid_name = spl[len(spl) - 1]
+       spl1 = vid_name.split('.')
+       vname = spl1[0]
+       final_name = vname + '_traj.json'
+       pathToTrajFile = dest_folder + final_name  # devo avere messo lo slah in pathToTrajDir !
 
-        write_json(array, pathToTrajFile)
-
+       write_json(array, pathToTrajFile)
+    # for i in range(len(array)):
+    #    print(array[i], '\n')
 
 # folder = 'D:\Dataset_Evasive_Movements\datasets\images_dataset\'
-def folder_process(folder, i_start, point_past, point_future, origin_distance):
+def folder_process(folder, step, i_start, point_past, point_future, origin_distance):
     print(folder)
     print(os.listdir(folder))
     dirs = os.listdir(folder)
@@ -122,7 +130,7 @@ def folder_process(folder, i_start, point_past, point_future, origin_distance):
             print("\t json folder: ", json_folder)
             video_json_path = json_folder + s + '.json'  # .../video*.json
             print("\t video json path: ", video_json_path)
-            create_traj_json(video_json_path, i_start, point_past, point_future, origin_distance, json_folder)
+            create_traj_json(video_json_path, step, i_start, point_past, point_future, origin_distance, json_folder)
 
 
 def main():
@@ -130,11 +138,13 @@ def main():
 
     parser.add_argument("--video_json_path", dest="input", default=None, help="Path of the json file of a video")
     parser.add_argument("--i_start", dest="start", default=0, help="Initial system origin")
+    parser.add_argument("--step", dest="step", default=10, help="Step")
+
     parser.add_argument("--point_past", dest="past", default=5,
                         help="Number of past points to remap in the new reference system")
-    parser.add_argument("--point_future", dest="future", default=30,
+    parser.add_argument("--point_future", dest="future", default=10,
                         help="Number of future points to remap in the new reference system")
-    parser.add_argument("--origin_distance", dest="origin_distance", default=10,
+    parser.add_argument("--origin_distance", dest="origin_distance", default=4,
                         help="Distance between two successive origins")
 
     parser.add_argument("--dest_folder", dest="dest", default=None,
@@ -146,13 +156,14 @@ def main():
     # point_future=int(args.future),origin_distance=int(args.origin_distance))
     if os.path.isdir(args.input):
         # esecuzione su cartella (e sottocartelle)
-        folder_process(folder=args.input, i_start=int(args.start), point_past=int(args.past),
+        folder_process(folder=args.input, step=args.step, i_start=int(args.start), point_past=int(args.past),
                        point_future=int(args.future), origin_distance=int(args.origin_distance))
     else:
         # esecuzione singolo video
-        create_traj_json(video_json_path=args.input, i_start=int(args.start), point_past=int(args.past),
+        create_traj_json(video_json_path=args.input, step=args.step, i_start=int(args.start), point_past=int(args.past),
                          point_future=int(args.future), origin_distance=int(args.origin_distance), dest_folder=args.dest)
 
 
 if __name__ == '__main__':
     main()
+
