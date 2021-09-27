304a3b226dfe4d4b875824be17a497ef244b716a
Chiara Albisani <chiaraalbi46@gmail.com>
2021-09-27 16:04:03 +0200

    Fix.

--- train_comet.py
+++ train_comet.py
@@ -36,8 +36,9 @@ def train(model, criterion, optimizer, train_loader, val_loader, epochs, val_per
     val_losses = {}
     train_losses = {}
 
-    device = torch.device('cuda:' + dev)
+    # device = torch.device('cuda:' + dev)
     # device = torch.device('cuda')
+    device = torch.device('cuda', int(dev))
 
     torch.cuda.reset_max_memory_allocated(device)
     model.to(device)
