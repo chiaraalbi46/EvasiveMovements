2e36ba1078f56ee9f292043d53e684e1b5f51aac
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2021-10-05 11:23:34 +0200

    Fix.

--- trajectories_draw.py
+++ trajectories_draw.py
@@ -1,8 +1,12 @@
 import numpy as np
 import json
-#from utils.read_csv import load_data_for_video
-import pyzed.sl as sl
+# from utils.read_csv import load_data_for_video
+import os
 import argparse
+import math
+import cv2
+from matplotlib import pyplot as plt
+
 
 # fx : Focal length in pixels along x axis.
 # cx : Optical center along x axis, defined in pixels (usually close to width/2).
@@ -18,113 +22,99 @@ def get_coordinates(path_json):
             frame_index.append(d[i]['Frame'])
     return data, frame_index
 
-def image_coordinates(path_json):
-    init_params = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,  # Use HD720 video mode (default fps: 60)
-                                    coordinate_units=sl.UNIT.METER,
-                                    coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
-    # Create a ZED camera object
-    zed = sl.Camera()
-    camera_pose = sl.Pose()  # zed_pose
+
+def image_coordinates():  # video_path, path_calib_j, path_json):
+
+    with open('C:/Users/ninad/Desktop/video_guida/video257.json') as json_file:
+         d = json.load(json_file)
+         xdata = []
+         ydata = []
+         zdata = []
+         for i in range(len(d)):
+             xdata.append(d[i]['cords'][0])
+             ydata.append(d[i]['cords'][1])
+             zdata.append(d[i]['cords'][2])
+
+    # immagine path
+    path = 'C:/Users/ninad/Desktop/video_guida/frame0010.png'
+
+    # # Focal length of the left eye in pixels
+    # focal = 675
+    # #focal_m = 0.0212   # metri
+    #
+    # optical_x = 654
+    # optical_y = 370
+    #
+    # # optical_x_m = (optical_x * focal_m) / focal
+    # # optical_y_m = (optical_y * focal_m) / focal
+    # # #
+    # # K = np.array([[round(focal_m, 2), 0, round(optical_x_m, 2)],
+    # #                [0, round(focal_m, 2), round(optical_y_m, 2)],
+    # #               [0, 0, 1]])
+    #
+    # K = np.array([[round(focal, 2), 0, round(optical_x, 2), 0],
+    #                              [0, round(focal, 2), round(optical_y, 2), 0],
+    #                              [0, 0, 1, 0]])
+    #
+    #
+    # vn = os.path.split(video_path)
+    # video_name = vn[1].split('.')
+    # video_name = video_name[0].split('_')[0]
+    #
+
+    # Matrice di proiezione
+    P = np.array([[675,   0, 654,   0], [0, 675, 370,   0], [0,   0,   1,   0]])
+    #P = np.array([[675, 0, 654, 0], [0, 675, 370,  -675], [0, 0, 1, 0]])
+
+    array_image_c =  []
+    for i in range(len(xdata)):  # cambia per ogni video
+        new_point_pj = np.dot(P, [xdata[i], -1, zdata[i], 1])
+        array_image_c.append(new_point_pj/new_point_pj[2])
+    print('array', array_image_c)
+
+    img = cv2.imread(path.strip(), -1)
+    img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+    x, y = 660, 600  # (x, y) coordinate punto corrente
+    cv2.circle(img, (x, y), 6, (0, 255, 0), 4)
+    width = 1280
+    for i in array_image_c:
+        x = int(i[0])
+        y = int(i[1])
+        #img[y - 1:y + 1, x - 1:x + 1] = [0, 0, 255]
+        cv2.circle(img, (x, y), 6, (0, 0, 255), 4)
+        print('x', x, 'y', y)
+
+        # if (j < (len(array_image_c) - 1)):
+        #             next_x = math.ceil(array_image_c[j + 1][0])
+        #             next_y = math.ceil(array_image_c[j + 1][1])
+        #             cv2.line(img, (x, y), (next_x, next_y), (0, 0, 255), 2)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+
+    # Save image
+    #cv2.imwrite("result.png", img)
+
+    ax = plt.axes()
+
+    #plt.plot(np.array(future)[get_ind_m][:, 0], np.array(future)[get_ind_m][:, 1], color="magenta", marker=".")  # , linestyle="")
+    plt.plot(np.array(array_image_c)[:, 0], np.array(array_image_c)[:, 1], color="green", marker=".")  # , linestyle="")
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    #plt.ylim([0, -8])
+    #plt.xlim([-4, 4])
+    # plt.gca().invert_yaxis() # origine asse z (y) in basso a sinistra
+    plt.show()
+   #  #return array_image_c
 
 
-    # Open the camera
-    status = zed.open(init_params)
-    if status != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
-        exit()
-
-    # Enable positional tracking with default parameters
-    tracking_params = sl.PositionalTrackingParameters()
-    zed.enable_positional_tracking(tracking_params)
-
-    cam_params = zed.get_camera_information().calibration_parameters
-    # Focal length of the left eye in pixels
-    focal_x = cam_params.left_cam.fx
-    focal_y = cam_params.left_cam.fy
-
-    optical_x = cam_params.left_cam.fx
-    optical_y = cam_params.left_cam.fy
-
-    K = np.array([[focal_x,       0,  optical_x],
-                  [      0, focal_y,  optical_y],
-                  [      0,       0,         1]])
-
-    #R = camera_pose.get_rotation_matrix(sl.Rotation()).r
-    #t = camera_pose.get_translation(sl.Translation()).get()
-    #world2cam = np.hstack((R, np.dot(-R, t).reshape(3,-1)))
-    # todo usare file csv con dati predetti
-
-    #path_frame = 'C:/Users/ninad/Desktop/video_guida/json/video29_traj.json'
-    future, frame_ind = get_coordinates(path_json)
-    # ora faccio solo su un frame : selezionare frame_path e coordinate
-
-    trasf_cord = []
-    for i in range(len(future)):
-        if frame_ind[i] == 330:
-            trasf_cord = future[i]
-
-
-    array_image_c = []
-    for i in range(len(trasf_cord)):
-        P = np.dot(K, [trasf_cord[i][0], trasf_cord[i][1], 1] )
-        array_image_c.append(P)
-    print('array_image', array_image_c)
-
-# def save_video(video_name, csv_path, len_seq, model_type):
-#     data_df = pd.read_csv(csv_path, error_bad_lines=False, names=["image_path", "predicted", "real"])
-#
-#     image_path = data_df['image_path'].values
-#     predicted, real = load_data_for_video(csv_path, len_seq)
-#     video_img = []
-#
-#     for i in tqdm.tqdm(range(len(image_path))):
-#
-#         img = cv2.imread(image_path[i].strip(), -1)
-#         img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
-#         height, width, _ = img.shape
-#         size = (width, height)
-#
-#         x, y = 600, 450  # (x, y) coordinate punto corrente
-#         cv2.circle(img, (x, y), 3, (0, 255, 0), 1)
-#
-#         for j, item in enumerate(predicted[i]):
-#             x = math.ceil(item[0])
-#             y = math.ceil(item[1])
-#             img[y - 1:y + 1, x - 1:x + 1] = [0, 0, 255]
-#             if (j < (len(predicted[i]) - 1)):
-#                 next_x = math.ceil(predicted[i][j + 1][0])
-#                 next_y = math.ceil(predicted[i][j + 1][1])
-#                 cv2.line(img, (x, y), (next_x, next_y), (0, 0, 255), 2)
-#
-#         for j, item in enumerate(real[i]):
-#             x = int(item[0])
-#             y = int(item[1])
-#             img[y - 1:y + 1, x - 1:x + 1] = [255, 255, 255]
-#             if (j < (len(real[i]) - 1)):
-#                 next_x = int(real[i][j + 1][0])
-#                 next_y = int(real[i][j + 1][1])
-#                 cv2.line(img, (x, y), (next_x, next_y), (255, 255, 255), 2)
-#
-#         font = cv2.FONT_HERSHEY_SIMPLEX
-#         cv2.putText(img, 'CNN+LSTM', (100, 100), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
-#         cv2.putText(img, 'Ground Truth', (100, 150), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
-#         cv2.putText(img, 'Starting Point', (100, 200), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-#
-#         video_img.append(img)
-#
-#     writer = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'DIVX'), 10, size)
-#     for i in tqdm.tqdm(range(len(video_img))):
-#         writer.write(video_img[i])
-#     writer.release()
-#     print()
-#     print("Create video and store it in " + video_name)
-
-#/home/aivdepth/datasets/images_dataset/normal/video29/left_frames_processed
-def main():
-    parser = argparse.ArgumentParser(description="Create the trajectories' file from a json file of a video sequence")
-    parser.add_argument("--json_path", dest="json_p", default=None, help="Path of the json file of trajectory")
-    args = parser.parse_args()
-    image_coordinates(path_json=args.json_p)
+# python trajectories_draw.py --json_path /home/aivdepth/datasets/images_dataset/normal/video29/
+# def main():
+#     parser = argparse.ArgumentParser(description="Create the trajectories' file from a json file of a video sequence")
+#     parser.add_argument("--json_path", dest="json_p", default=None, help="Path of the json file of trajectory")
+#     args = parser.parse_args()
+#     image_coordinates(path_json=args.json_p)
 
 if __name__ == "__main__":
-    main()
+    # main()
+    image_coordinates()
